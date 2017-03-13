5c2948fde6e7e9d1ba610921f8556cb94c921dfb
Will <will.sampaio13@outlook.com>
2017-03-13 16:41:59 -0300

    Teste final OK

--- app/src/main/java/com/bigsoftware/filadeespera/MenuActivity.java
+++ app/src/main/java/com/bigsoftware/filadeespera/MenuActivity.java
@@ -26,6 +26,7 @@ public class MenuActivity extends AppCompatActivity {
         //teste ok
         //teste ok de novo
         //teste final
+        //teste final ok
         MedicoNEG medicoNEG = new MedicoNEG(MenuActivity.this);
 
         medicoNEG.deletarMedicos();
